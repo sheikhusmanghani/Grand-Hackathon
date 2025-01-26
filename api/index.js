6946bea8b990ff84ae4a530858be7234530ccc48
Hafiz-Usman-Ghani-123 <hafizusmanghani22322@gmail.com>
2025-01-26 08:55:15 +0500

    olk

--- api/index.js
+++ api/index.js
@@ -1,8 +1,20 @@
+require("dotenv").config();
 const express = require("express");
+const connectDB = require("./config/connect-mongo");
 const app = express();
+const userCreate = require("./routes/signup");
+const cors = require("cors");
 
-app.get("/", (req, res) => res.send("Express on Vercel"));
+// middlewares
+app.use(cors());
+app.use(express.json());
 
-app.listen(3000, () => console.log("Server ready on port 3000."));
+// connection
+connectDB();
+
+// routes
+app.use("/",userCreate);
+
+app.listen(process.env.PORT, () => console.log("Server ready on port 3000."));
 
 module.exports = app;
